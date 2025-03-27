df8496003bf8fb3f3cf39c8d03b9870f60cf9b85
lmlp playground <62958251+nilsen-hub@users.noreply.github.com>
2025-03-27 13:19:36 +0100

    day 19 cleanup

--- src/solutions/day_19.rs
+++ src/solutions/day_19.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, time::Instant};
+use std::time::Instant;
 
 #[derive(Debug, Clone)]
 struct InputData {
@@ -20,7 +20,7 @@ impl InputData {
 
                     towels.patterns = Graph::from(patterns);
                 }
-                1 => (),
+                1 => continue,
                 _ => towels.designs.push(line.chars().collect()),
             }
         }
@@ -28,7 +28,7 @@ impl InputData {
     }
 }
 
-#[derive(Debug, Clone, Default, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Default)]
 struct Graph {
     edges: Vec<Graph>, // [w, u, b, r, g, x];
     status: bool,
@@ -68,43 +68,38 @@ impl Graph {
             None => return false,
         }
     }
+
     // debug / test- function
-    fn print_data(
-        &self,
-        current: &Graph,
-        to_print: Vec<char>,
-        //collector: &mut Vec<Vec<char>>,
-    ) {
-        // debug: -> Vec<Vec<char>>
-        for (index, node) in current.edges.iter().enumerate() {
-            if node.status {
-                let mut to_print = to_print.clone();
-                if index != 5 {
-                    to_print.push(convert_index(&index));
-                }
-
-                match node.edges.get(5) {
-                    Some(n) => {
-                        if n.status {
-                            //collector.push(to_print.clone());
-                            println!("{:?}", &to_print);
-                        }
-                    }
-                    None => (),
-                }
-
-                self.print_data(&node, to_print); // , to_print, collector
-            }
-        }
-
-        //collector.clone()
-    }
-}
-
-#[derive(Debug, Default, Clone, PartialEq, Eq, Hash)]
-struct ForkRecord {
-    target: Vec<usize>,
-    position: Vec<Graph>,
+    //fn print_data(
+    //    &self,
+    //    current: &Graph,
+    //    to_print: Vec<char>,
+    //    //collector: &mut Vec<Vec<char>>,
+    //) {
+    //    // debug: -> Vec<Vec<char>>
+    //    for (index, node) in current.edges.iter().enumerate() {
+    //        if node.status {
+    //            let mut to_print = to_print.clone();
+    //            if index != 5 {
+    //                to_print.push(convert_index(&index));
+    //            }
+    //
+    //            match node.edges.get(5) {
+    //                Some(n) => {
+    //                    if n.status {
+    //                        //collector.push(to_print.clone());
+    //                        println!("{:?}", &to_print);
+    //                    }
+    //                }
+    //                None => (),
+    //            }
+    //
+    //            self.print_data(&node, to_print); // , to_print, collector
+    //        }
+    //    }
+    //
+    //    //collector.clone()
+    //}
 }
 
 #[derive(Debug, Default, Clone)]
@@ -119,34 +114,60 @@ impl Towels {
     fn solve_part_1(&self) -> usize {
         let mut acc = 0;
         for design in &self.designs {
-            if self.towel_check_dfs(&make_vec_usize(&design)) {
+            if self.towel_finder(&make_vec_usize(&design), &self.patterns) {
                 acc += 1;
             }
         }
         acc
     }
-    fn solve_part_2(&self) -> u32 {
-        let mut acc = 0;
-        let mut possible = 0;
-        for (index, design) in self.designs.iter().enumerate() {
-            if self.towel_check_dfs(&make_vec_usize(&design)) {
-                let v = self.towel_check_bfs(&make_vec_usize(&design));
-                if v > 0 {
-                    possible += 1;
+
+    fn towel_finder(&self, target: &Vec<usize>, graph: &Graph) -> bool {
+        let mut index = 0;
+        let mut graph_history: Vec<usize> = Vec::with_capacity(100);
+        let mut current_graph = &graph.edges;
+        let mut graph_history_seen: Vec<usize> = Vec::with_capacity(80);
+
+        loop {
+            if index == target.len() {
+                if current_graph[5].status {
+                    return true;
                 }
-                acc += v;
-                //match v {
-                //    0 => println!("Design {} is IMPOSSIBLE!!!!!!!!", index + 1),
-                //    1 => println!("Design {} has {} possible configuration", index + 1, v),
-                //    _ => println!("Design {} has {} possible configurations", index + 1, v),
-                //}
-            } //else {
-            //    println!("Design {} is impossible", index + 1);
-            //}
+                match graph_history.pop() {
+                    Some(v) => {
+                        index = v;
+                        current_graph = &self.patterns.edges;
+                    }
+                    None => return false,
+                }
+            }
+
+            if current_graph[target[index]].status {
+                current_graph = &current_graph[target[index]].edges;
+                index += 1;
+                if current_graph[5].status {
+                    if !graph_history_seen.contains(&index) {
+                        graph_history.push(index);
+                        graph_history_seen.push(index);
+                    }
+                }
+                continue;
+            }
+
+            if !current_graph[5].status {
+                match graph_history.pop() {
+                    Some(v) => {
+                        index = v;
+                        current_graph = &self.patterns.edges;
+                        continue;
+                    }
+                    None => return false,
+                }
+            } else {
+                current_graph = &self.patterns.edges;
+            }
         }
-        println!("Possible: {}", possible);
-        acc
     }
+
     fn towel_check_bfs(&self, target: &Vec<usize>) -> u32 {
         // the idea here ties my brain into a knot, but it should be simple enough
         // I take the target, and send it through the towel checker.
@@ -165,117 +186,12 @@ impl Towels {
 
         0
     }
-    fn towel_check_dfs_ass(&self, graph: &Vec<Graph>, target: &Vec<usize>) -> bool {
-        let mut index = 0;
-        let mut graph_history: Vec<usize> = Vec::new();
-        let mut current_graph = graph;
-        let mut graph_history_seen: Vec<usize> = Vec::with_capacity(80);
-
-        loop {
-            if index == target.len() {
-                if current_graph[5].status {
-                    return true;
-                }
-                match graph_history.pop() {
-                    Some(v) => {
-                        graph_history_seen.push(v);
-                        index = v;
-                        current_graph = &graph;
-                    }
-                    None => return false,
-                }
-            }
-
-            if current_graph[target[index]].status {
-                current_graph = &current_graph[target[index]].edges;
-                index += 1;
-                if current_graph[5].status {
-                    if !graph_history_seen.contains(&index) {
-                        graph_history.push(index);
-                    }
-                }
-                continue;
-            }
-
-            if !current_graph[5].status {
-                match graph_history.pop() {
-                    Some(v) => {
-                        graph_history_seen.push(v);
-                        index = v;
-                        current_graph = &graph;
-                        continue;
-                    }
-                    None => return false,
-                }
-            } else {
-                current_graph = &graph;
-            }
-        }
-    }
-
-    fn towel_check_dfs(&self, target: &Vec<usize>) -> bool {
-        let mut index = 0;
-        let mut graph_history: Vec<usize> = Vec::new();
-        let mut current_graph = &self.patterns.edges;
-        let mut graph_history_seen: Vec<usize> = Vec::with_capacity(80);
-
-        loop {
-            if index == target.len() {
-                if current_graph[5].status {
-                    return true;
-                }
-                match graph_history.pop() {
-                    Some(v) => {
-                        graph_history_seen.push(v);
-                        index = v;
-                        current_graph = &self.patterns.edges;
-                    }
-                    None => return false,
-                }
-            }
-
-            if current_graph[target[index]].status {
-                current_graph = &current_graph[target[index]].edges;
-                index += 1;
-                if current_graph[5].status {
-                    if !graph_history_seen.contains(&index) {
-                        graph_history.push(index);
-                    }
-                }
-                continue;
-            }
-
-            if !current_graph[5].status {
-                match graph_history.pop() {
-                    Some(v) => {
-                        graph_history_seen.push(v);
-                        index = v;
-                        current_graph = &self.patterns.edges;
-                        continue;
-                    }
-                    None => return false,
-                }
-            } else {
-                current_graph = &self.patterns.edges;
-            }
-        }
-    }
 }
 
-fn test_graph_completeness(graph_output: Vec<Vec<char>>, mut graph_input: HashSet<Vec<char>>) {
-    println!(
-        "collector len, input len: {}, {}",
-        graph_output.len(),
-        graph_input.len()
-    );
-    for line in graph_output {
-        if graph_input.remove(&line) {
-            println!("present in both sets: {:?}", line);
-        } else {
-            println!("phantom vector: {:?}", line);
-        }
-    }
-    println!("Should be zero: {}", graph_input.len());
+#[derive(Debug, Default, Clone)]
+struct ForkRecord {
+    target: Vec<usize>,
+    position: Vec<Graph>,
 }
 
 fn make_vec_usize(vec: &Vec<char>) -> Vec<usize> {
@@ -296,16 +212,6 @@ fn convert_color(c: &char) -> usize {
         _ => panic!("expected different value"),
     }
 }
-fn convert_index(i: &usize) -> char {
-    match i {
-        0 => 'w',
-        1 => 'u',
-        2 => 'b',
-        3 => 'r',
-        4 => 'g',
-        _ => panic!("expected different value"),
-    }
-}
 
 fn part_1(input: &InputData) {
     let now = Instant::now();
@@ -319,7 +225,7 @@ fn part_2(input: &InputData) {
     let now = Instant::now();
     let towels = input.parse();
 
-    println!("Part two: {}", towels.solve_part_2());
+    //println!("Part two: {}", towels.solve_part_2());
     println!("Runtime (micros): {}", now.elapsed().as_micros());
 }
 
@@ -335,16 +241,45 @@ pub fn solution(data: &str, test_data: &str) {
     println!("Day nineteen answers:");
     println!("");
 
-    //part_1(&test);
     part_1(&input);
     println!("");
     part_2(&input);
     println!("");
 }
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn parser_works() {}
-}
+// testing functions below
+
+//fn convert_index(i: &usize) -> char {
+//    match i {
+//        0 => 'w',
+//        1 => 'u',
+//        2 => 'b',
+//        3 => 'r',
+//        4 => 'g',
+//        _ => panic!("expected different value"),
+//    }
+//}
+
+//fn test_graph_completeness(graph_output: Vec<Vec<char>>, mut graph_input: HashSet<Vec<char>>) {
+//    println!(
+//        "collector len, input len: {}, {}",
+//        graph_output.len(),
+//        graph_input.len()
+//    );
+//    for line in graph_output {
+//        if graph_input.remove(&line) {
+//            println!("present in both sets: {:?}", line);
+//        } else {
+//            println!("phantom vector: {:?}", line);
+//        }
+//    }
+//    println!("Should be zero: {}", graph_input.len());
+//}
+
+//#[cfg(test)]
+//mod tests {
+//    use super::*;
+//
+//    #[test]
+//    fn parser_works() {}
+//}
